799052b30faccb7366421fe3777e755e11f0aec1
TermNew <nso5@hotmail..com>
2026-07-14 20:28:17 +0300

    Initial Commit

---
+++ Jb/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Jb
+//
+//  Created by س on 29/01/1448 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Jb/JbApp.swift
@@ -0,0 +1,17 @@
+//
+//  JbApp.swift
+//  Jb
+//
+//  Created by س on 29/01/1448 AH.
+//
+
+import SwiftUI
+
+@main
+struct JbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
